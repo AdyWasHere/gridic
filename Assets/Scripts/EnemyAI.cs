f1c46a224eb97bddac373cdabc613e08e913ffc0
AdyWasHere <34395710+AdyWasHere@users.noreply.github.com>
2018-02-08 10:43:21 +0200

    Added game parts
    Added the things I made so far
    + glow
    + movement l/r
    + simple collision

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+	bool right; //right collider to trigger movement <-
+	bool left; // left collider to triggr movement ->
+
+	[SerializeField]
+	float movementSpeed;
+
+
+	void Start () {
+		left = false;
+		right = true;
+	}
+	
+
+	void Update () {
+		if (left) {
+			transform.position += Vector3.right * movementSpeed * Time.fixedDeltaTime;
+		
+
+		}
+		if (right) {
+			transform.position += Vector3.left * movementSpeed * Time.fixedDeltaTime;
+
+		}
+	}
+
+
+	public void OnTriggerEnter(Collider col)
+	{
+
+		if (col.gameObject.tag == "leftcollider") {
+
+			left = true;
+			right = false;
+
+		}
+
+
+		if (col.gameObject.tag == "rightcollider") {
+			right = true;
+			left = false;
+
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public Rigidbody playerRb;
+
+	bool canJump;
+
+
+	[SerializeField]
+	float jumpSpeed;
+
+	[SerializeField]
+	float movementSpeed;
+
+	void Start () {
+		canJump = false;
+
+	}
+
+	void FixedUpdate()
+	{
+		#if (UNITY_EDITOR)
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * movementSpeed * Time.fixedDeltaTime;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * movementSpeed * Time.fixedDeltaTime;
+		}
+		#endif
+
+	
+	}
+
+	void Update()
+	{
+
+		if (canJump) {
+
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+
+				Jump();
+			}
+
+		}
+
+	}
+
+	/*void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		playerRb.AddForce (movement * movementSpeed);
+
+	}*/
+
+	public void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "platform") {
+
+			canJump = true;
+
+
+		}
+
+	}
+
+	void Jump()
+	{
+		playerRb.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
+		canJump = false;
+	}
+}
